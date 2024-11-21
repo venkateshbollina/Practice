dd92783e3e6f89b1fcc8fab685368cd6663b5692
B venkatesh <130389006+venkateshbollina@users.noreply.github.com>
2024-11-21 15:24:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using System;
+
+public class main
+{
+
+    public static void Main()
+    {
+        int[] array = new int[6] { 1, 2, 33, 4, 57, 6 };
+        string[] cars = new string[4] { "tata", "suzuki", "toyota", "mahendra" };
+
+        foreach (int i in array )
+        {
+            Console.WriteLine(i);
+           
+        }
+        Array.Sort(array);
+        foreach (int i in array )
+        Console.WriteLine(i);
+
+        foreach (string  i in cars) Console.WriteLine(i);
+
+
+    }
+}
