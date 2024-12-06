1c39d5181a4471adc1eabcae7b5a50c754533493
B venkatesh <130389006+venkateshbollina@users.noreply.github.com>
2024-12-06 19:04:56 +0530

    Add files via upload

---
+++ Task2/Text.json/Text.json/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Text.Json;
+using System;
+
+namespace Text.json {
+
+    public class Program
+    {
+        employee employee = new employee()
+        {
+            FirstName = "sunny",
+            LastName = "sss",
+            Dob = DateTime.Parse("2001-12-12"),
+            EmployeeId = 1,
+            Gender="male",
+            Salary=200000
+
+        };
+
+        var json = jsonSerializer.Serialize(employee);
+
+
+
+    }
+
+
+}
---
+++ Task2/Text.json/Text.json/employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text.json
+{
+    public class employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public double Salary { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+    }
+}
